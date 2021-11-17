c98ac0ee37a5b103b06b2e843edb1cb26cc77ed3
Samy Hamidou <admin@datalo.net>
2021-11-17 10:41:31 +0100

    added: outputs.tf in demo3

---
+++ aws-stack-demo3/outputs.tf
@@ -0,0 +1,21 @@
+output "elb_id" {
+  description = "The name of the ELB"
+  value       = concat(aws_elb.autoscalling_group_elb_Web.*.id, [""])[0]
+}
+
+
+
+output "elb_name" {
+  description = "The name of the ELB"
+  value       = concat(aws_elb.autoscalling_group_elb_Web.*.name, [""])[0]
+}
+
+output "elb_dns_name" {
+  description = "The DNS name of the ELB"
+  value       = concat(aws_elb.autoscalling_group_elb_Web.*.dns_name, [""])[0]
+}
+
+output "elb_instances" {
+  description = "The list of instances in the ELB"
+  value       = flatten(aws_elb.autoscalling_group_elb_Web.*.instances)
+}
