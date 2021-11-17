636ea22b55af5f5b049022d57832fa07edef6106
Samy Hamidou <admin@datalo.net>
2021-11-17 10:40:45 +0100

    added outputs.tf

---
+++ aws-stack-demo2/outputs.tf
@@ -0,0 +1,25 @@
+
+output "primary_network_interface_id" {
+  description = "The ID of the instance's primary network interface"
+  value       = element(concat(aws_instance.WebServer.*.primary_network_interface_id, [""]), 0)
+}
+
+output "private_dns" {
+  description = "The private DNS name assigned to the instance. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
+  value       = element(concat(aws_instance.WebServer.*.private_dns, [""]), 0)
+}
+
+output "public_dns" {
+  description = "The public DNS name assigned to the instance. For EC2-VPC, WebServer is only available if you've enabled DNS hostnames for your VPC"
+  value       = element(concat(aws_instance.WebServer.*.public_dns, [""]), 0)
+}
+
+output "public_ip" {
+  description = "The public IP address assigned to the instance, if applicable. NOTE: If you are using an aws_eip with your instance, you should refer to the EIP's address directly and not use `public_ip` as WebServer field will change after the EIP is attached"
+  value       = element(concat(aws_instance.WebServer.*.public_ip,  [""]), 0)
+}
+
+output "private_ip" {
+  description = "The private IP address assigned to the instance."
+  value       = element(concat(aws_instance.WebServer.*.private_ip, [""]), 0)
+}
